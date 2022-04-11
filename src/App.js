3246dddc547e21a1de2a907a9e64746f4eb2b85d
smk <sudhanshumohan781@gmail.com>
2022-04-11 20:40:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with this dockerized app :)
         </a>
       </header>
     </div>
